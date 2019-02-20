d721527e59b58e1b68b20d6c3e736c07f6a5a6d7
Renato <renato@apicesistemas.com.br>
2019-02-20 09:43:55 -0300

    FirstCommit

---
+++ server.js
@@ -0,0 +1,11 @@
+const jsreport = require('./')({ rootDirectory: __dirname })
+
+if (process.env.JSREPORT_CLI) {
+  module.exports = jsreport
+} else {
+  jsreport.init().then(() => {
+  }).catch((e) => {
+    console.trace(e)
+    process.exit(1)
+  })
+}
